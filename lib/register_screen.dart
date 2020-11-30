0d8f938997586e9cd8d6a71ec47232bba959a26b
Miguel693 <miguelag_2002@hotmail.com>
2020-11-29 22:30:10 -0300

    input_page.dart reemplazado por register_screen.dart

---
+++ lib/register_screen.dart
@@ -0,0 +1,200 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  String _nombre;
+  String _email = '';
+  String _password = '';
+  String _fecha = "";
+
+  List<String> _provincias = [
+    "Provincia",
+    'Neuquen',
+    "Mendoza",
+    "Rio Negro",
+    "Santa Fe",
+    "Tierra del Fuego"
+  ];
+
+  String opcionSeleccionada = 'Provincia';
+
+  TextEditingController _inputFieldDateController = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Registro"),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        children: <Widget>[
+          _crearInput(),
+          Divider(),
+          _crearDNI(),
+          Divider(),
+          _crearEmail(),
+          Divider(),
+          _crearPassword(),
+          Divider(),
+          _crearFecha(context),
+          Divider(),
+          _crearDropdown(),
+          Divider(),
+          _crearPersona(),
+        ],
+      ),
+    );
+  }
+
+  Widget _crearInput() {
+    return TextField(
+      autofocus: false,
+      textCapitalization: TextCapitalization.words,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        // counter: Text("Letras ${_nombre.length}"),
+        labelText: 'Nombre(s) y Apelldo(s)',
+        hintText: 'Ejemplo Pepito Muñoz',
+        icon: Icon(Icons.account_circle),
+      ),
+      onChanged: (valor) {
+        setState(() {
+          _nombre = valor;
+        });
+      },
+    );
+  }
+
+  //
+  Widget _crearEmail() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        hintText: 'ejemplo@dominio.com',
+        labelText: 'Email',
+        icon: Icon(Icons.email),
+      ),
+      onChanged: (valor) {
+        setState(() {
+          _email = valor;
+        });
+      },
+    );
+  }
+
+  Widget _crearDNI() {
+    return TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        hintText: '00000000',
+        labelText: 'DNI',
+        icon: Icon(Icons.assignment_ind),
+      ),
+    );
+  }
+
+  Widget _crearPassword() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        hintText: 'Password',
+        labelText: 'Password',
+        icon: Icon(Icons.lock),
+      ),
+      onChanged: (valor) {
+        setState(() {
+          _password = valor;
+        });
+      },
+    );
+  }
+
+  Widget _crearFecha(BuildContext context) {
+    return TextField(
+      controller: _inputFieldDateController,
+      enableInteractiveSelection: false,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        hintText: 'Fecha de Nacimiento',
+        labelText: 'Fecha de Nacimiento',
+        icon: Icon(Icons.calendar_today),
+      ),
+      onTap: () {
+        FocusScope.of(context).requestFocus(new FocusNode());
+        _selectDate(context);
+      },
+    );
+  }
+
+  _selectDate(BuildContext context) async {
+    DateTime picked = await showDatePicker(
+      context: context,
+      firstDate: new DateTime(1960),
+      initialDate: new DateTime.now(),
+      lastDate: new DateTime(2025),
+      locale: Locale('es', 'AR'),
+    );
+
+    if (picked != null) {
+      setState(() {
+        _fecha = picked.toString();
+        _inputFieldDateController.text = _fecha;
+      });
+    }
+  }
+
+  List<DropdownMenuItem<String>> getOpcionesDropdown() {
+    List<DropdownMenuItem<String>> lista = new List();
+    _provincias.forEach((opcionSeleccionada) {
+      lista.add(DropdownMenuItem(
+        child: Text(opcionSeleccionada),
+        value: opcionSeleccionada,
+      ));
+    });
+
+    return lista;
+  }
+
+  Widget _crearDropdown() {
+    return Row(
+      children: <Widget>[
+        Icon(Icons.select_all),
+        SizedBox(width: 30.0),
+        Expanded(
+          child: DropdownButton(
+            value: opcionSeleccionada,
+            items: getOpcionesDropdown(),
+            onChanged: (opt) {
+              setState(() {
+                opcionSeleccionada = opt;
+              });
+            },
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _crearPersona() {
+    return ListTile(
+      title: Text('Nombre es $_nombre'),
+      subtitle: Text('Email es $_email'),
+      trailing: Text(opcionSeleccionada),
+    );
+  }
+}
