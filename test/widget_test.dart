da1610aa793eea384810f60088bf723a037499d5
Miguel693 <miguelag_2002@hotmail.com>
2020-11-30 19:48:44 -0300

    Errores en el archivo eliminados, no corregidos

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:proyecto_citamedic/login1.dart';
+// import 'package:proyecto_citamedic/login1.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    // await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
